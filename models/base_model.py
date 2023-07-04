d15974ba1cce987166b78bb494135601f119f5c4
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-04 12:53:08 -0700

    feat: adding files about base class and test files

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This is our class base for this proyect"""
+import uuid
+import datetime
+
+
+class BaseModel:
+    """This class has attributes and methods"""
+    id = str(uuid.uuid4())
+    created_at = datetime.datetime(2023, 7, 3, 13, 25, 13, 45, None)
+    updated_at = datetime.datetime(2023, 7, 3, 13, 25, 13, 45, None)
+
+    def __str__(self):
+        """Print"""
+        dic = dict(self.__dict__, **{'id': self.id,
+               'created_at': self.created_at, 'updated_at':
+               self.updated_at})
+        return f"[{self.__class__.__name__}] ({self.id}) {dic})"
+
+    def save(self):
+        """updates the public instance attribute updated_at
+        with the current datetime"""
+        self.updated_at = datetime.datetime(2023, 7, 3, 13, 25, 13, 45, None)
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of
+        __dict__ of the instance"""
+        return dict(self.__dict__, **{'id': self.id, '__class__':
+               self.__class__.__name__, 'created_at':
+               self.created_at.isoformat(), 'updated_at':
+               self.updated_at.isoformat()})
