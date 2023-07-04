dfa7e080f1f7cef2502d965dcc2292fd868e4c4e
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-04 14:56:06 -0700

    fix: fixing error with attributes publicof instance

--- models/base_model.py
+++ models/base_model.py
@@ -6,15 +6,12 @@ import datetime
 
 class BaseModel:
     """This class has attributes and methods"""
-    id = str(uuid.uuid4())
-    created_at = datetime.datetime.now()
-    updated_at = datetime.datetime.now()
-
     def __init__(self, *args, **kwargs):
         """Arguments for the constructor dof a BaseModel"""
-        if not kwargs and not args:
-            id = str(uuid.uuid4())
-            created_at = datetime.datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
         else:
             for keys, value in kwargs.items():
                 if keys == 'id':
