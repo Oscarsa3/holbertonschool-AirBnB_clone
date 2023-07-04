76df5ecb77fc5c3e4eb31e1da3921411b7b14b7f
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-04 14:42:03 -0700

    feat: update this file adding method constructor with args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,24 @@ class BaseModel:
     created_at = datetime.datetime.now()
     updated_at = datetime.datetime.now()
 
+    def __init__(self, *args, **kwargs):
+        """Arguments for the constructor dof a BaseModel"""
+        if not kwargs and not args:
+            id = str(uuid.uuid4())
+            created_at = datetime.datetime.now()
+        else:
+            for keys, value in kwargs.items():
+                if keys == 'id':
+                    self.id = value
+                elif keys == 'created_at':
+                    self.created_at = datetime.datetime.fromisoformat(value)
+                elif keys == 'updated_at':
+                    self.updated_at = datetime.datetime.fromisoformat(value)
+
     def __str__(self):
         """Print"""
-        dic = dict(self.__dict__, **{'id': self.id,
-               'created_at': self.created_at, 'updated_at':
-               self.updated_at})
+        dic = dict(self.__dict__, **{'id': self.id, 'created_at':
+                   self.created_at, 'updated_at': self.updated_at})
         return f"[{self.__class__.__name__}] ({self.id}) {dic})"
 
     def save(self):
@@ -26,6 +39,6 @@ class BaseModel:
         """returns a dictionary containing all keys/values of
         __dict__ of the instance"""
         return dict(self.__dict__, **{'id': self.id, '__class__':
-               self.__class__.__name__, 'created_at':
-               self.created_at.isoformat(), 'updated_at':
-               self.updated_at.isoformat()})
+                    self.__class__.__name__, 'created_at':
+                    self.created_at.isoformat(), 'updated_at':
+                    self.updated_at.isoformat()})
