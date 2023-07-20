3f5964fb9f9f749a9d66fd523a836b55faecc87a
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-07-19 22:04:52 -0500

    removing some methods that not use

--- console.py
+++ console.py
@@ -162,26 +162,6 @@ class HBNBCommand(cmd.Cmd):
         except IndexError:
             return None
 
-    def parseline(self, line):
-        """Parse the line into a command name and a string containing
-        the arguments.  Returns a tuple containing (command, args, line).
-        'command' and 'args' may be None if the line couldn't be parsed.
-        """
-        line = line.strip()
-        if not line:
-            return None, None, line
-        elif line[0] == '?':
-            line = 'help ' + line[1:]
-        elif line[0] == '!':
-            if hasattr(self, 'do_shell'):
-                line = 'shell ' + line[1:]
-            else:
-                return None, None, line
-        i, n = 0, len(line)
-        while i < n and line[i] in self.identchars: i = i+1
-        cmd, arg = line[:i], line[i:].strip()
-        return cmd, arg, line
-
 def separarArgs(arg):
     """separamos la linea leida en string separados"""
     argumentos = arg.split()
